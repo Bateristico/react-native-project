c0d1e6089f64d9812288e9bca9b2bfd3724a9595
Bateristico <camilo.saa@gmail.com>
2022-01-20 20:38:03 +0100

    rendering a flat list

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
-  return <Text>List Screen</Text>;
+  const pets = [
+    { name: 'Mirka' },
+    { name: 'Lonia' },
+    { name: 'Laika' },
+    { name: 'Chispa' },
+    { name: 'Yuya' }
+  ];
+  return (
+    <FlatList
+      data={pets}
+      renderItem={({ item }) => {
+        return <Text>{item.name}</Text>;
+      }}
+    ></FlatList>
+  );
 };
 
 const styles = StyleSheet.create({});
