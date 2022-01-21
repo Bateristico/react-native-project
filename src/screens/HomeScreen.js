962f31c6b18a8849431868b52a12a3b5ecbfae58
Bateristico <camilo.saa@gmail.com>
2022-01-21 21:47:19 +0100

    add touchabe opacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View>
       <Text style={styles.a}>Native Project!</Text>
       <Button onPress={() => console.log('button pressed!!')} title="Go to Components Demo" />
+      <TouchableOpacity onPress={() => console.log('list pressed!!')}>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
